001e80e51b8bc1e41c3919f4ccb16ecf35e061d8
santanamnaa <santanamnaa8@gmail.com>
2023-12-07 17:14:16 +0700

    menambahkan satu dua tiga

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>git remote</title>
 </head>
+
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nemo tempore tenetur vel rerum impedit eveniet expedita porro atque magnam. Error explicabo neque, quos voluptate vel sapiente officia optio dignissimos?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
+
 </html>
